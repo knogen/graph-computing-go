6d5792f90b8906f0d2aaab7f240c26748c6d4306
ider <admin@knogen.cn>
2024-12-31 17:36:00 +0800

    添加学术圈

--- internal/wikiEntropy/wikiSubjectEntropy.go
+++ internal/wikiEntropy/wikiSubjectEntropy.go
@@ -22,10 +22,8 @@ func MainSubject() {
 	wg := sync.WaitGroup{}
 	for year := 2003; year <= 2024; year += 1 {
 
-		for _, subjectTitle := range subjectList {
-
-			for level := 1; level <= 3; level++ {
-
+		for level := 1; level <= 3; level++ {
+			for _, subjectTitle := range subjectList {
 				wg.Add(1)
 				pool.Submit(func() {
 					tag := fmt.Sprintf("lv%d-%s-%d", level, subjectTitle, year)
@@ -61,6 +59,52 @@ func MainSubject() {
 					wg.Done()
 				})
 			}
+
+			// academic circle
+			wg.Add(1)
+			pool.Submit(func() {
+				var revisionChan = make(chan *extractwikipediadump.PageInMongo, 10)
+				var pageMap = make(map[int64]*extractwikipediadump.PageInMongo)
+				plhwg := sync.WaitGroup{}
+				plhwg.Add(1)
+				go func() {
+					pageMap = pageLinkHandle(revisionChan)
+					plhwg.Done()
+				}()
+
+				for _, subjectTitle := range subjectList {
+					tag := fmt.Sprintf("lv%d-%s-%d", level, subjectTitle, year)
+					log.Info().Any("subjectTag", tag).Msg("graph entropy start")
+					subRevisionChan, err := mongoClient.Get_pages_subject_cat(tag, 0)
+					if err != nil {
+						log.Fatal().Err(err).Msg("failed to get pages by year")
+					}
+					for item := range subRevisionChan {
+						revisionChan <- item
+					}
+				}
+				close(revisionChan)
+				plhwg.Wait()
+
+				var totalWikiItemSlice []*extractwikipediadump.PageInMongo
+				for _, item := range pageMap {
+					if item.Redirect != nil {
+						continue
+					}
+					totalWikiItemSlice = append(totalWikiItemSlice, item)
+				}
+				subjectTitle := "academic circle"
+				graphSci := getWorksGraph(totalWikiItemSlice)
+
+				entropy1 := graphSci.DegreeEntropy()
+				mongoClient.InsertSubjectEntropy(year, len(graphSci.Nodes), graphSci.EdgeCount, subjectTitle, "degree", level, entropy1)
+
+				entropy2 := graphSci.StructEntropy()
+				mongoClient.InsertSubjectEntropy(year, len(graphSci.Nodes), graphSci.EdgeCount, subjectTitle, "struct", level, entropy2)
+
+				log.Info().Any("subjectTag", subjectTitle).Any("graph Nodes", len(graphSci.Nodes)).Msg("graph entropy complete")
+				wg.Done()
+			})
 
 		}
 	}
